369ded4fed0685fd8d08cce31549228e9db7a38c
Saturnalli <129856415+Saturnalli@users.noreply.github.com>
2024-06-09 13:35:28 +0800

    Changed Scripts location.

---
+++ js/app.js
@@ -0,0 +1,14 @@
+  document.getElementById('image').addEventListener('change', function(event) {
+            const file = event.target.files[0];
+            if (file) {
+                const reader = new FileReader();
+                reader.onload = function(e) {
+                    const preview = document.getElementById('image-preview');
+                    preview.src = e.target.result;
+                    preview.style.display = 'block';
+                }
+                reader.readAsDataURL(file);
+            } else {
+                document.getElementById('image-preview').style.display = 'none';
+            }
+        });
